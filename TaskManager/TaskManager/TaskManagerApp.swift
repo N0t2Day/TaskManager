97a69ab446104b2623073cba2a5f6388d1c07ebc
 Artem Kedrov <artemkedrov1991@gmail.com>
2024-12-04 12:26:35 +0200

    + project initialize

---
+++ TaskManager/TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Artem Kedrov on 04.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskManager/TaskManagerTests/TaskManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerTests.swift
+//  TaskManagerTests
+//
+//  Created by Artem Kedrov on 04.12.2024.
+//
+
+import Testing
+@testable import TaskManager
+
+struct TaskManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
