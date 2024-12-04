7091a08b422407242e8521d368eecfaf73a9013b
 Artem Kedrov <artemkedrov1991@gmail.com>
2024-12-04 13:11:14 +0200

    + Mock user data creation refactoring

--- TaskManager/TaskManager/Models/UserTask.swift
+++ TaskManager/TaskManager/Models/UserTask.swift
@@ -16,6 +16,11 @@ struct UserTask: Codable, Identifiable {
 
 extension UserTask {
     static var mock: UserTask {
-        .init(id: UUID().uuidString, title: String(), text: String(), createdAt: Date())
+        let titleConfig = RandomStringGenerator.Config(length: 7, options: [.uppercase])
+        let textConfig = RandomStringGenerator.Config(length: 512, options: RandomStringGenerator.Option.allCases)
+        
+        let title = RandomStringGenerator.getNewRandomString(with: titleConfig)
+        let text = RandomStringGenerator.getNewRandomString(with: textConfig)
+        return UserTask(id: UUID().uuidString, title: title, text: text, createdAt: Date())
     }
 }
