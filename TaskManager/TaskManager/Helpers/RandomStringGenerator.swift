938cf0687199f989cd205fce79e6cf149cb75214
 Artem Kedrov <artemkedrov1991@gmail.com>
2024-12-04 13:06:12 +0200

    + Random String generator refactoring

--- TaskManager/TaskManager/Helpers/RandomStringGenerator.swift
+++ TaskManager/TaskManager/Helpers/RandomStringGenerator.swift
@@ -8,10 +8,16 @@
 import Foundation
 
 enum RandomStringGenerator {
+    struct Config {
+        let length: Int
+        let options: [Option]
+    }
+    
     enum Option: CaseIterable {
         case lowercase
         case uppercase
         case numeric
+        case whitespace
         
         var letters: String {
             switch self {
@@ -21,12 +27,18 @@ enum RandomStringGenerator {
                 return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
             case .numeric:
                 return "0123456789"
+            case .whitespace:
+                return " "
             }
         }
     }
     
-    static func getNewRandomString(with length: Int, options: Option...) -> String {
+    static func getNewRandomString(with length: Int, options: [Option]) -> String {
         let letters = options.map { $0.letters }.joined()
         return String((0..<length).map{ _ in letters.randomElement()! })
+    }
+    
+    static func getNewRandomString(with config: Config) -> String {
+        getNewRandomString(with: config.length, options: config.options)
     }
 }
